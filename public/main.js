2f4a8b9d4dac175a7e82906d0a4436c7f9502f53
Sijia <xSijia.Li@gmail.com>
2021-11-17 20:44:57 -0500

    more react

--- public/main.js
+++ public/main.js
@@ -1,20 +1,20 @@
 const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
-    // Create the browser window.
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
 
-    //load the index.html from a url
-    win.loadURL('http://localhost:3000');
+  // load the index.html from a url
+  win.loadURL('http://localhost:3000')
 
-    // Open the DevTools.
-    win.webContents.openDevTools()
+  // Open the DevTools.
+  win.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
@@ -26,18 +26,18 @@ app.whenReady().then(createWindow)
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
 app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
 
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
-    }
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
 })
 
 // In this file you can include the rest of your app's specific main process
